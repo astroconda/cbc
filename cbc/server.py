3807de830b82897792554c3785f7153a46d53f51
Joseph Hunkeler <jhunkeler@gmail.com>
2015-06-23 21:19:27 -0400

    Running the file server is now optional

--- cbc/server.py
+++ cbc/server.py
@@ -4,14 +4,19 @@ import socketserver
 
 
 class FileServer(object):
-    def __init__(self, port, root=os.curdir):
+    def __init__(self, port, root=os.path.abspath(os.curdir), run=False):
         if isinstance(port, str):
             port = int(port)
-            
+
+        self.root = root
         self.port = port
         self.handler = http.server.SimpleHTTPRequestHandler
 
+        if run:
+            self.run()
+
     def run(self):
+        os.chdir(self.root)
         httpd = socketserver.TCPServer(("", self.port), self.handler)
         print("{0} active on port {1}".format(self.__class__.__name__, self.port))
-        httpd.serve_forever()
+        httpd.handle_request()
